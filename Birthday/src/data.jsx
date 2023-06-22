4eb339c15d1c032c5498275e1607caa907f913a1
sushantbro11 <119164210+sushantbro11@users.noreply.github.com>
2023-06-22 17:14:20 +0545

    Add files via upload

---
+++ Birthday/src/data.jsx
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    name: 'Max',
+    age: 6,
+    image:
+      'https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg',
+  },
+  {
+    id: 2,
+    name: 'Luna',
+    age: 7,
+    image:
+      'https://media.cnn.com/api/v1/images/stellar/prod/220818142713-dogs-tears-emotions-wellness-stock.jpg?c=3x2',
+  },
+  {
+    id: 3,
+    name: 'Larry',
+    age: 5,
+    image:
+      'https://scx2.b-cdn.net/gfx/news/2019/4-dog.jpg',
+  },
+  {
+    id: 4,
+    name: 'Sean',
+    age: 4,
+    image:
+      'https://www.nylabone.com/-/media/project/oneweb/nylabone/images/dog101/activities-fun/10-great-small-dog-breeds/maltese-portrait.jpg',
+  },
+];
