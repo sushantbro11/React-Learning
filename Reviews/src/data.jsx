7dd1465fb5281d9ba8076bfe8a199cd0090f171d
sushantbro11 <119164210+sushantbro11@users.noreply.github.com>
2023-06-27 19:10:21 +0545

    Add files via upload

---
+++ Reviews/src/data.jsx
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Lionel Messi",
+    position: "Forward",
+    image:
+      "https://s.abcnews.com/images/Sports/wirestory_2fef42117e73c5943816f7b079b698c2_16x9_992.jpg",
+    description:
+      "Lionel Messi is an Argentine professional footballer who plays as a forward. He is often considered one of the greatest players of all time. Messi has won numerous awards and titles throughout his career, including multiple FIFA Ballon d'Or awards.",
+  },
+  {
+    id: 2,
+    name: "Cristiano Ronaldo",
+    position: "Forward",
+    image:
+      "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt4a28b9f98b99984e/641cc85f73a6bb6c4d25f0f6/Cristiano_Ronaldo_Portugal_2023.jpg?auto=webp&format=pjpg&width=3840&quality=60",
+    description:
+      "Cristiano Ronaldo is a Portuguese professional footballer who plays as a forward. He is known for his incredible goal-scoring ability and athleticism. Ronaldo has achieved great success at both the club and international levels, winning numerous trophies and individual accolades.",
+  },
+  {
+    id: 3,
+    name: "Neymar Jr.",
+    position: "Forward",
+    image:
+      "https://cdn.resfu.com/media/img_news/agencia-efe_multimedia_55010692278.multimedia.photos.55010692278003.file.jpg?size=1000x&lossy=1",
+    description:
+      "Neymar Jr. is a Brazilian professional footballer who plays as a forward. Known for his skill, creativity, and flair on the pitch, Neymar has established himself as one of the most exciting players in the world. He has played for top clubs such as Barcelona and Paris Saint-Germain.",
+  },
+  {
+    id: 4,
+    name: "Sergio Ramos",
+    position: "Defender",
+    image:
+      "https://i2-prod.mirror.co.uk/incoming/article30144123.ece/ALTERNATES/s1200c/0_Sergio-Ramos-2.jpg",
+    description:
+      "Sergio Ramos is a Spanish professional footballer who plays as a defender. He is known for his strong defensive skills, leadership qualities, and ability to score goals from set-pieces. Ramos has had a highly successful career at Real Madrid, winning numerous trophies with the club.",
+  },
+];
+
+export default reviews;
